2104fb481f8a4ce454b64b10a6e101db76e36589
fredrik-bakke <fredrbak@gmail.com>
2022-07-26 13:42:30 +0200

    construct numbers digit by digit, add . and +

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const zero = "+[]";
 const one = "+!+[]";
 
-const number = n => {
-  if (n === 0) return zero;
-  if (n === 1) return one;
+const unaryNumber = n => {
+  if (n == 0) return zero;
+  if (n == 1) return one;
   return Array.from({ length: n }, () => "!+[]").join('+');
 }
 
+const listNumber = n => {
+  let s = [];
+  for (digit of n + [])
+    s.push(unaryNumber(digit));
+  return "[" + s.join("]+[") + "]";
+}
+
+const number = n => {
+  if (n < 10)
+    return unaryNumber(n);
+  return `+[${listNumber(n)}]`;
+}
+console.log(number(10));
 
 const map = {};
 
@@ -16,13 +29,15 @@ const fromChar = x => {
   const charCode = x.charCodeAt(0);
   return `([]+[])[${fromString("constructor")}][${fromString("fromCharCode")}](${number(charCode)})`;
 }
-const fromString = s => s.split('').map(fromChar).join('+');
+const fromString = s => s.split("").map(fromChar).join('+');
 
 
 for (let i = 0; i < 10; i++)
   map[i+[]] = number(i)+"+[]";
 
-map["10"] = "+[[+!+[]]+[+[]]]";
+map["10"] = "[+!+[]]+[+[]]+[]";
+map["11"]= "[+!+[]]+[+!+[]]+[]";
+map["100"]= "[+!+[]]+[+[]]+[+[]]+[]";
 
 
 //[object Object]//console.log({}+[]);
@@ -54,16 +69,19 @@ map.u = `([]+[][[]])[${number(0)}]`;
 map.n = `([]+[][[]])[${number(1)}]`;
 map.d = `([]+[][[]])[${number(2)}]`;
 
+// console.log(eval(`${fromString("11e100")}`));
+// map['.'] = 
+
 //function RegExp() { [native code] }//console.log(eval(`([]+(/!/)[${fromString('constructor')}])`));
-map.R = `([]+(/!/)[${fromString('constructor')}])[${number(9)}]`;
-map.g = `([]+(/!/)[${fromString('constructor')}])[${number(11)}]`;
-map.E = `([]+(/!/)[${fromString('constructor')}])[${number(12)}]`;
-map.x = `([]+(/!/)[${fromString('constructor')}])[${number(13)}]`;
-map.p = `([]+(/!/)[${fromString('constructor')}])[${number(14)}]`;
-map.v = `([]+(/!/)[${fromString('constructor')}])[${number(25)}]`;
-map['}'] = `([]+(/!/)[${fromString('constructor')}])[${number(34)}]`;
-//function String() { [native code] }//console.log(eval(`([]+([]+[])[${fromString('constructor')}])`));
-map.S = `([]+([]+[])[${fromString('constructor')}])[${number(9)}]`;
+map.R = `([]+(/!/)[${fromString("constructor")}])[${number(9)}]`;
+map.g = `([]+(/!/)[${fromString("constructor")}])[${number(11)}]`;
+map.E = `([]+(/!/)[${fromString("constructor")}])[${number(12)}]`;
+map.x = `([]+(/!/)[${fromString("constructor")}])[${number(13)}]`;
+map.p = `([]+(/!/)[${fromString("constructor")}])[${number(14)}]`;
+map.v = `([]+(/!/)[${fromString("constructor")}])[${number(25)}]`;
+map['}'] = `([]+(/!/)[${fromString("constructor")}])[${number(34)}]`;
+//function String() { [native code] }//console.log(eval(`([]+([]+[])[${fromString("constructor")}])`));
+map.S = `([]+([]+[])[${fromString("constructor")}])[${number(9)}]`;
 ///\\\\///console.log(/\\\\/+[]);
 map['\\'] = `([]+/\\\\/)[${number(1)}]`;
 map['/'] = `([]+/!/)[${number(0)}]`;
@@ -90,31 +108,35 @@ map['%'] = `((()=>{})[${fromString("constructor")}](${fromString("return escape"
 map.C = `((()=>{})[${fromString("constructor")}](${fromString("return escape")})()(${map['\\']}))[${number(2)}]`;
 //NaN//console.log(+{}+[]);
 map.N = `(+{}+[])[${number(0)}]`;
+map["1.1e+101"] = `+[[+!+[]]+[+!+[]]+[${map.e}]+[+!+[]]+[+[]]+[+[]]]+[]`;
+//1.1e+101//console.log(eval(`+[[+!+[]]+[+!+[]]+[${map.e}]+[+!+[]]+[+[]]+[+[]]]+[]`));
+map['.'] = `(+[[+!+[]]+[+!+[]]+[${map.e}]+[+!+[]]+[+[]]+[+[]]]+[])[${number(1)}]`;
+map['+'] = `(+[[+!+[]]+[+!+[]]+[${map.e}]+[+!+[]]+[+[]]+[+[]]]+[])[${number(4)}]`;
+console.log()
+
+
+console.log("constructor length: " + fromString("constructor").length);
 
 const compile = code => `(()=>{})[${fromString("constructor")}](${fromString(code)})()`;
 
 const code = compile("console.log(\"Hello world!\");");
-console.log(code);
 //console.log(code);
-console.log(code.length);
+console.log("Hello world length: " + code.length);
 eval(code);
 
 
-let present = "";
 let missing = "";
 
 for (let letter of "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/\\()[]{}!@#$%^&*_-+=|:;<>,.?/'\"`~") {
-  if (letter in map)
-    present += letter;
-  else
+  if (!(letter in map))
     missing += letter;
 }
 console.log(missing);
 
-for (letter of present) {
-  let actual = eval(map[letter]);
-  if (actual != letter)
-    console.error(`The map for '${letter}' is wrong! It evaluated to ${actual}.`);
+for (const [key, value] of Object.entries(map)) {
+  let actual = eval(value);
+  if (actual !== key)
+    console.error(`The map for '${key}' is wrong! It evaluated to ${actual}.`);
 }
 
 const perf = chars => {
@@ -124,8 +146,12 @@ const perf = chars => {
   return cum / chars.length;
 }
 
-let presentPerf = perf(present);
-console.log(`There are ${present.length} mapped characters. Average code length is ${Math.round(presentPerf)}.`);
+let mapPerf = 0;
+for (const [key, value] of Object.entries(map))
+  mapPerf += value.length;
+
+mapPerf /= Object.entries(map).length;
+console.log(`There are ${Object.entries(map).length} mapped characters. Average code length is ${Math.round(mapPerf)}.`);
 let missingPerf = perf(missing);
 console.log(`There are ${missing.length} mapped characters. Average code length is ${Math.round(missingPerf)}.`);
 
