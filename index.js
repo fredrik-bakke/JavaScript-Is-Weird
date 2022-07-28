5ea1c1f491d204f6ee9fbc242115dba3afb66885
fredrik-bakke <fredrbak@gmail.com>
2022-07-28 13:52:30 +0200

    Optimize number encodings

--- index.js
+++ index.js
@@ -14,34 +14,58 @@ const listNumber = n => {
   return "[" + s.join("]+[") + "]";
 }
 
-const number = n => {
-  if (n < 10)
-    return unaryNumber(n);
-  return `+(${listNumber(n)})`;
+function strNumber(n) {
+  let s = [];
+  for (digit of n + [])
+    s.push(unaryNumber(digit));
+
+  return s[0] + "+[" + s.slice(1).join("]+[") + "]";
+}
+
+// Use when the difference between number and string is unimportant
+function number(n) {
+  let s = [];
+  for (digit of n + [])
+    s.push(unaryNumber(digit));
+
+  if (s.length == 1)
+    return s[0];
+  else
+    return "+(" + s[0] + "+[" + s.slice(1).join("]+[") + "]" + ")";
+}
+
+function sNumber(n) {
+  let s = [];
+  for (digit of n + [])
+    s.push(unaryNumber(digit));
+
+  if (s.length == 1)
+    return s[0];
+  else
+    return s[0] + "+[" + s.slice(1).join("]+[") + "]";
 }
 
 const map = {};
-
 const words = {}; // For strings of connected characters
 
 function checkWords(str, pos) {
-  for (const [key,value] of Object.entries(words))
+  for (const [key, value] of Object.entries(words))
     if (str.startsWith(key, pos))
-      return {word: key, wordCode: value};
+      return { word: key, wordCode: value };
   return null;
 }
 const encodeChar = x => {
   if (x in map)
     return map[x];
   const charCode = x.charCodeAt(0);
-  return `([]+[])[${encodeChars("constructor")}][${encodeChars("fromCharCode")}](${number(charCode)})`;
+  return `([]+[])[${encodeChars("constructor")}][${encodeChars("fromCharCode")}](${sNumber(charCode)})`;
 }
 const encodeChars = s => s.split("").map(encodeChar).join("+");
 
 
 const encode = s => {
   const tokens = [];
-  for(let i = 0; i < s.length; i++) {
+  for (let i = 0; i < s.length; i++) {
     let pair = checkWords(s, i);
     if (pair !== null) {
       tokens.push(pair.wordCode);
@@ -56,106 +80,102 @@ const encode = s => {
 
 
 for (let i = 0; i < 10; i++)
-  map[i + []] = number(i) + "+[]";
+  map[i + []] = strNumber(i);
 
 //[object Object]//console.log({}+[]);
-map['['] = `([]+{})[${number(0)}]`;
-map.o = `([]+{})[${number(1)}]`;
-map.b = `([]+{})[${number(2)}]`;
-map.j = `([]+{})[${number(3)}]`;
-map.c = `([]+{})[${number(5)}]`;
-map[' '] = `([]+{})[${number(7)}]`;
-map.O = `([]+{})[${number(8)}]`;
-map[']'] = `([]+{})[${number(14)}]`;
+map['['] = `([]+{})[${sNumber(0)}]`;
+map.o = `([]+{})[${sNumber(1)}]`;
+map.b = `([]+{})[${sNumber(2)}]`;
+map.j = `([]+{})[${sNumber(3)}]`;
+map.c = `([]+{})[${sNumber(5)}]`;
+map[' '] = `([]+{})[${sNumber(7)}]`;
+map.O = `([]+{})[${sNumber(8)}]`;
+map[']'] = `([]+{})[${sNumber(14)}]`;
 
 words["false"] = "![]+[]";
-map.f = `([]+![])[${number(0)}]`;
-map.a = `([]+![])[${number(1)}]`;
-map.l = `([]+![])[${number(2)}]`;
-map.s = `([]+![])[${number(3)}]`;
+map.f = `([]+![])[${sNumber(0)}]`;
+map.a = `([]+![])[${sNumber(1)}]`;
+map.l = `([]+![])[${sNumber(2)}]`;
+map.s = `([]+![])[${sNumber(3)}]`;
 
 words["true"] = "!![]+[]";
-map.t = `([]+!![])[${number(0)}]`;
-map.r = `([]+!![])[${number(1)}]`;
-map.e = `([]+!![])[${number(3)}]`;
+map.t = `([]+!![])[${sNumber(0)}]`;
+map.r = `([]+!![])[${sNumber(1)}]`;
+map.e = `([]+!![])[${sNumber(3)}]`;
 
 words["NaN"] = "+{}+[]";
-map.N = `(+{}+[])[${number(0)}]`;
+map.N = `(+{}+[])[${sNumber(0)}]`;
 
 words["Infinity"] = "+!![]/+[]+[]"
-map.I = `(+!![]/+[]+[])[${number(0)}]`;
-map.i = `(+!![]/+[]+[])[${number(3)}]`;
-map.y = `(+!![]/+[]+[])[${number(7)}]`;
+map.I = `(+!![]/+[]+[])[${sNumber(0)}]`;
+map.i = `(+!![]/+[]+[])[${sNumber(3)}]`;
+map.y = `(+!![]/+[]+[])[${sNumber(7)}]`;
 
 words["undefined"] = "[]+[][[]]";
-map.u = `([]+[][[]])[${number(0)}]`;
-map.n = `([]+[][[]])[${number(1)}]`;
-map.d = `([]+[][[]])[${number(2)}]`;
+map.u = `([]+[][[]])[${sNumber(0)}]`;
+map.n = `([]+[][[]])[${sNumber(1)}]`;
+map.d = `([]+[][[]])[${sNumber(2)}]`;
 
 
-map.m = `([]+(+[])[${encode("constructor")}])[${number(11)}]`;
+map.m = `([]+(+[])[${encode("constructor")}])[${sNumber(11)}]`;
 
 // Names
 words["Array"] = `[][${encode("constructor")}][${encode("name")}]`;
+map.A = `([]+[][${encode("constructor")}])[${sNumber(9)}]`;
+
 words["Number"] = `(+[])[${encode("constructor")}][${encode("name")}]`;
+
 words["Boolean"] = `(![])[${encode("constructor")}][${encode("name")}]`;
-words["String"] = `([]+[])[${encode("constructor")}][${encode("name")}]`;
+map.B = `([]+(![])[${encode("constructor")}])[${sNumber(9)}]`;
+
+
 words["Function"] = `[][${encode("flat")}][${encode("constructor")}][${encode("name")}]`;
+map.F = `([]+[][${encode("flat")}][${encode("constructor")}])[${sNumber(9)}]`;
+
+
 words["String"] = `([]+[])[${encode("constructor")}][${encode("name")}]`;
-words["RegExp"] = `([]+/!/[${encode("constructor")}])[${encode("name")}]`;
-
-
+map.S = `([]+([]+[])[${encode("constructor")}])[${sNumber(9)}]`;
+map.v = `([]+[][${encode("flat")}])[${sNumber(23)}]`;
 
 //function RegExp() { [native code] }//console.log(eval(`([]+(/!/)[${fromString('constructor')}])`));
-map.R = `([]+/!/[${encode("constructor")}])[${number(9)}]`;
-map.g = `([]+/!/[${encode("constructor")}])[${number(11)}]`;
-map.E = `([]+/!/[${encode("constructor")}])[${number(12)}]`;
-map.x = `([]+/!/[${encode("constructor")}])[${number(13)}]`;
-map.p = `([]+/!/[${encode("constructor")}])[${number(14)}]`;
-//map['}'] = `([]+(/!/)[${fromString("constructor")}])[${number(34)}]`;
-//function String() { [native code] }//console.log(eval(`([]+([]+[])[${fromString("constructor")}])`));
-map.S = `([]+([]+[])[${encode("constructor")}])[${number(9)}]`;
-map.v = `([]+[][${encode("flat")}])[${number(23)}]`; 
+words["RegExp"] = `([]+/!/[${encode("constructor")}])[${encode("name")}]`;
+map.R = `([]+/!/[${encode("constructor")}])[${sNumber(9)}]`;
+map.g = `([]+/!/[${encode("constructor")}])[${sNumber(11)}]`;
+map.E = `([]+/!/[${encode("constructor")}])[${sNumber(12)}]`;
+map.x = `([]+/!/[${encode("constructor")}])[${sNumber(13)}]`;
+map.p = `([]+/!/[${encode("constructor")}])[${sNumber(14)}]`;
+
 ///\\\\///console.log(/\\\\/+[]);
-map['\\'] = `([]+/\\\\/)[${number(1)}]`;
-map['/'] = `([]+/!/)[${number(0)}]`;
-map['!'] = `([]+/!/)[${number(1)}]`;
-map['('] = `([]+/()/)[${number(1)}]`;
-map[')'] = `([]+/()/)[${number(2)}]`;
-map['{'] = `([]+/{/)[${number(1)}]`;
-map['}'] = `([]+/}/)[${number(1)}]`;
-map['='] = `([]+/=/)[${number(1)}]`;
-map['>'] = `([]+/>/)[${number(1)}]`;
+map['\\'] = `([]+/\\\\/)[${sNumber(1)}]`;
+map['/'] = `([]+/!/)[${sNumber(0)}]`;
+map['!'] = `([]+/!/)[${sNumber(1)}]`;
+map['('] = `([]+/()/)[${sNumber(1)}]`;
+map[')'] = `([]+/()/)[${sNumber(2)}]`;
+map['{'] = `([]+/{/)[${sNumber(1)}]`;
+map['}'] = `([]+/}/)[${sNumber(1)}]`;
+map['='] = `([]+/=/)[${sNumber(1)}]`;
+map['>'] = `([]+/>/)[${sNumber(1)}]`;
 //map['<'] = `([]+/</)[${number(1)}]`;
 //map['"'] = `([]+/"/)[${number(1)}]`;
 //map['\''] = `([]+/'/)[${number(1)}]`;
 
-
-map.h = `(${number(17)})[${encode("toString")}](${number(20)})`;
-map.k = `(${number(20)})[${encode("toString")}](${number(21)})`;
-map.q = `(${number(26)})[${encode("toString")}](${number(30)})`;
-map.w = `(${number(32)})[${encode("toString")}](${number(33)})`;
-map.z = `(${number(35)})[${encode("toString")}](${number(36)})`;
+map.h = `(${number(17)})[${encode("toString")}](${sNumber(20)})`;
+map.k = `(${number(20)})[${encode("toString")}](${sNumber(21)})`;
+map.q = `(${number(26)})[${encode("toString")}](${sNumber(30)})`;
+map.w = `(${number(32)})[${encode("toString")}](${sNumber(33)})`;
+map.z = `(${number(35)})[${encode("toString")}](${sNumber(36)})`;
 
 //%5C//console.log(eval(`(()=>{})[${fromString('constructor')}](${fromString('return escape')})()(${map['\\']})`));
-map['%'] = `((()=>{})[${encode("constructor")}](${encode("return escape")})()(${map['\\']}))[${number(0)}]`;
-map.C = `((()=>{})[${encode("constructor")}](${encode("return escape")})()(${map['\\']}))[${number(2)}]`;
+map['%'] = `((()=>{})[${encode("constructor")}](${encode("return escape")})()(${map['\\']}))[${sNumber(0)}]`;
+map.C = `((()=>{})[${encode("constructor")}](${encode("return escape")})()(${map['\\']}))[${sNumber(2)}]`;
 
 
-//1.1e+101//console.log(eval(`+[[+!+[]]+[+!+[]]+[${map.e}]+[+!+[]]+[+[]]+[+[]]]+[]`));
-map['.'] = `(+[${listNumber(11)}+[${map.e}]+${listNumber(20)}]+[])[${number(1)}]`;
-map['+'] = `(+[${listNumber(11)}+[${map.e}]+${listNumber(20)}]+[])[${number(4)}]`;
+//1.1e+21//console.log(eval(`+[[+!+[]]+[+!+[]]+[${map.e}]+[+!+[]]+[+[]]+[+[]]]+[]`));
+map['.'] = `(+(${sNumber(11)}+[${map.e}]+${listNumber(20)})+[])[${sNumber(1)}]`;
+map['+'] = `(+(${sNumber(1)}+[${map.e}]+${listNumber(21)})+[])[${sNumber(2)}]`;
 
-
-
-console.log("constructor length: " + encode("constructor").length);
 
 const compile = code => `(()=>{})[${encode("constructor")}](${encode(code)})()`;
-
-const code = compile("console.log('Hello world!');");
-//console.log(code);
-console.log("Hello world length: " + code.length);
-eval(code);
 
 
 let missing = "";
@@ -187,6 +207,18 @@ mapPerf /= Object.entries(map).length;
 console.log(`There are ${Object.entries(map).length} mapped characters. Average code length is ${Math.round(mapPerf)}.`);
 let missingPerf = perf(missing);
 console.log(`There are ${missing.length} non-mapped characters. Average code length is ${Math.round(missingPerf)}.`);
+
+
+
+
+const code = compile("console.log('Hello world!');");
+// console.log(code);
+
+console.log("Hello world length: " + code.length);
+eval(code);
+
+
+
 
 
 
